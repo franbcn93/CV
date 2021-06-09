1f3cf4146e57048488da05f3048a2d14a0eda18d
Francisco <franciscoparisalbero@gmail.com>
2021-06-09 10:43:51 +0200

    Updates

---
+++ precache-manifest.f5daead56728e241a2fd83f0fbb44079.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73eb430f086f4ea67291635c1b7c5ed0",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "67b12ec332573378365c",
+    "url": "/CV/static/css/main.fceb7bca.chunk.css"
+  },
+  {
+    "revision": "4b509c75e0706efe5f62",
+    "url": "/CV/static/js/2.e5565095.chunk.js"
+  },
+  {
+    "revision": "0f45e0275f167abf997a7ab25d0f76de",
+    "url": "/CV/static/js/2.e5565095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b12ec332573378365c",
+    "url": "/CV/static/js/main.1937f78d.chunk.js"
+  },
+  {
+    "revision": "8f2e2295ed9a838f16a3",
+    "url": "/CV/static/js/runtime-main.c2b36a22.js"
+  },
+  {
+    "revision": "45d1cf13e04bca8f13669aa8712a834e",
+    "url": "/CV/static/media/imagen2.45d1cf13.png"
+  },
+  {
+    "revision": "50c90d56e7a705f4cdeca3ba7f332980",
+    "url": "/CV/static/media/imagen3.50c90d56.jpg"
+  },
+  {
+    "revision": "e2e1273c7d1495ec1f33036309641a4d",
+    "url": "/CV/static/media/imagen3_1.e2e1273c.gif"
+  },
+  {
+    "revision": "d432adc1d5d60e3056ccd85ed17cb0b1",
+    "url": "/CV/static/media/imagen6.d432adc1.jpg"
+  },
+  {
+    "revision": "bfbd90a54ae6a9d354fb01c99fac5e18",
+    "url": "/CV/static/media/imagen8.bfbd90a5.jpg"
+  }
+]);
